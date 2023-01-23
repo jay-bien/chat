434ba4e34a90ad11203c2a6b6123aaed30ff3b1e
jay-bien <jay.bienaime@gmail.com>
2023-01-23 16:44:25 -0500

    Added typing func to simulate bot typing response char by char

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 // Import stylesheets
 import './style.css';
 
-import { LOADER_INTERVAL } from './constants';
+import { LOADER_INTERVAL, TYPE_SPEED } from './constants';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
@@ -19,4 +19,16 @@ function ellipseSpinner(el) {
     el.textContent += '.';
     if (el.textContent === '....') el.textContent = '';
   }, LOADER_INTERVAL);
+}
+
+function type(el, text) {
+  let idx = 0;
+  const interval = setInterval(() => {
+    if (idx < text.length) {
+      el.innerHtml += text.charAt(idx);
+      idx++;
+    } else {
+      clearInterval(interval);
+    }
+  }, TYPE_SPEED);
 }
