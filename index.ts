73b202f9cb4141f8c710e727d28ed83e6420cad4
jay-bien <jay.bienaime@gmail.com>
2023-01-23 16:17:19 -0500

    Added function to render ellipses loading to an element. Will render while Delphi is "thinking"

--- index.ts
+++ index.ts
@@ -4,4 +4,17 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 const titleDiv: HTMLElement = document.getElementById('title');
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
 titleDiv.innerHTML = `<h1>Delphi</h1>`;
+
+//
+ellipseSpinner(chatContainer);
+
+function ellipseSpinner(el) {
+  el.textContent = '';
+  const timer: ReturnType<typeof setInterval> = setInterval(() => {
+    el.textContent += '.';
+    if (el.textContent === '....') el.textContent = '';
+  }, 250);
+}
