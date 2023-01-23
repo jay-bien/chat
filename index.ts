461ecf891ce36cbb121afd9ecce4ec8f35186a65
jay-bien <jay.bienaime@gmail.com>
2023-01-23 17:36:13 -0500

    Added func to generate unique id based on timestamp+randomNumber

--- index.ts
+++ index.ts
@@ -14,19 +14,23 @@ ellipseSpinner(chatContainer);
 let div = document.createElement('div');
 chatContainer.appendChild(div);
 type(div, 'Let there be light.');
+let id = generateUniqueId();
+titleDiv.innerHTML += id;
 //eo sanity checks
 
-function ellipseSpinner(el: HTMLElement) {
+function ellipseSpinner(el: HTMLElement): ReturnType<typeof setInterval> {
   el.textContent = '';
   const timer: ReturnType<typeof setInterval> = setInterval(() => {
     el.textContent += '.';
     if (el.textContent === '....') el.textContent = '';
   }, LOADER_INTERVAL);
+
+  return timer;
 }
 
 function type(el: HTMLElement, text: string) {
   let idx = 0;
-  const interval = setInterval(() => {
+  const interval: ReturnType<typeof setInterval> = setInterval(() => {
     if (idx < text.length) {
       el.innerHTML += text.charAt(idx);
       idx++;
@@ -34,4 +38,12 @@ function type(el: HTMLElement, text: string) {
       clearInterval(interval);
     }
   }, TYPE_SPEED);
+}
+
+function generateUniqueId(): string {
+  const timestamp = Date.now();
+  const randomNum = Math.random();
+  const hexaString = randomNum.toString(16);
+  const id = `${timestamp}-${hexaString.padStart(18, '0')}`;
+  return id;
 }
