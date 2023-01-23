63c9a89734f690d5a76723141fc6cd3419477721
jay-bien <jay.bienaime@gmail.com>
2023-01-23 17:53:05 -0500

    added function to create chat bar

--- constants.ts
+++ constants.ts
@@ -1,3 +1,4 @@
 // todo Add option to dynamically set these
 export const LOADER_INTERVAL: number = 450; // sets speed of loader when bot is thinking
 export const TYPE_SPEED: number = 30; //sets typing speed of bot when responding
+export const BOT_NAME: string = 'Delphi';
--- index.ts
+++ index.ts
@@ -1,23 +1,27 @@
 // Import stylesheets
 import './style.css';
 
-import { LOADER_INTERVAL, TYPE_SPEED } from './constants';
+import { LOADER_INTERVAL, TYPE_SPEED, BOT_NAME } from './constants';
 
 const appDiv: HTMLElement = document.getElementById('app');
 const titleDiv: HTMLElement = document.getElementById('title');
 const form = document.querySelector('form');
 const chatContainer: HTMLElement = document.querySelector('#chat_container');
-titleDiv.innerHTML = `<h1>Delphi</h1>`;
 
-// sanity checks
+/* sanity checks */
+titleDiv.innerHTML = `<h1>Delphi</h1>`;
 ellipseSpinner(chatContainer);
 let div = document.createElement('div');
 chatContainer.appendChild(div);
 type(div, 'Let there be light.');
 let id = generateUniqueId();
 titleDiv.innerHTML += id;
-//eo sanity checks
 
+let newResponse = chatBar(false, 'Hello human', generateUniqueId());
+chatContainer.innerHTML += newResponse;
+/* eo sanity checks */
+
+//generate loading indicator and render to passed in el
 function ellipseSpinner(el: HTMLElement): ReturnType<typeof setInterval> {
   el.textContent = '';
   const timer: ReturnType<typeof setInterval> = setInterval(() => {
@@ -28,6 +32,7 @@ function ellipseSpinner(el: HTMLElement): ReturnType<typeof setInterval> {
   return timer;
 }
 
+// takes element, paragraph as args and adds para chars 1 by 1 to the el
 function type(el: HTMLElement, text: string) {
   let idx = 0;
   const interval: ReturnType<typeof setInterval> = setInterval(() => {
@@ -40,10 +45,24 @@ function type(el: HTMLElement, text: string) {
   }, TYPE_SPEED);
 }
 
+// see name
 function generateUniqueId(): string {
   const timestamp = Date.now();
   const randomNum = Math.random();
   const hexaString = randomNum.toString(16);
   const id = `${timestamp}-${hexaString.padStart(18, '0')}`;
   return id;
+}
+
+function chatBar(isUser: boolean, val: string, id: string): string {
+  //todo add ability to choose name and avatar by user
+  const bar = `
+  <div class="bar_wrap ${isUser ? 'user' : BOT_NAME}">
+    <div class="chat">
+      <div className="profile">${isUser ? 'User' : BOT_NAME}</div>
+      <div id="${id}" class="chat_message" >${val}</div>
+    </div>
+  </div>`;
+
+  return bar;
 }
